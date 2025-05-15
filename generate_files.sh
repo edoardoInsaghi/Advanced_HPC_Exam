4e91a90b83a2300c57d58d6ac8334a35236cb412
Edoardo Insaghi <72695438+edoardoInsaghi@users.noreply.github.com>
2025-05-15 16:45:16 +0200

    updating generate files

--- generate_files.sh
+++ generate_files.sh
@@ -3,7 +3,7 @@
 for PROCS in 1 2; do
   cat > jacobi_mpi_${PROCS}.sbatch <<EOF
 #!/bin/bash
-#SBATCH --job-name=jacobi_${PROCS}mpi
+#SBATCH --job-name=jacobi_${PROCS}_mpi
 #SBATCH --ntasks=${PROCS}
 #SBATCH --nodes=${PROCS}
 #SBATCH --cpus-per-task=128
@@ -18,7 +18,7 @@ for NX in 1000 10000; do
   for THREADS in 1 2 4 8 16 32 64 128; do
     echo "Running NX=\$NX THREADS=\$THREADS with ${PROCS} MPI processes"
     export OMP_NUM_THREADS=\$THREADS
-    mpirun -np ${PROCS} ./test_cpu \$NX \$THREADS
+    mpirun -np ${PROCS} --map-by node --bind-to none ./test_cpu \$NX \$THREADS
   done
 done
 EOF
